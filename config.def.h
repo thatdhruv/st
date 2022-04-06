929fa20c87e742b2d7d75931f58ba50622d5ad09
thatdhruv <45945622+thatdhruv@users.noreply.github.com>
2022-04-06 22:00:16 +0530

    Update config.def.h

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=18:antialias=true:autohint=true";
+static char *font = "Monaco:style=Regular:pixelsize=18:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
