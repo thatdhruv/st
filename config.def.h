3b303b099a460e43c638756900426bfb564b6d2d
thatdhruv <45945622+thatdhruv@users.noreply.github.com>
2022-04-07 19:32:34 +0530

    Update config.def.h

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Monaco:style=Regular:pixelsize=18:antialias=true:autohint=true";
+static char *font = "Monaco Nerd Font Mono:style=Regular:pixelsize=18:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
